56d9a401c94c3e1087cf36738a88fe9dfd1fe8a1
dipakp-logicrays <dipakp@logicrays.com>
2024-05-13 18:04:53 +0530

    React essential component props: 01 Making Components Reusable with Props [Core Concepts]

--- code/06-React-Essentials/02-component-props/src/App.jsx
+++ code/06-React-Essentials/02-component-props/src/App.jsx
@@ -1,5 +1,7 @@
 import logo from "./assets/react-core-concepts.png";
-import {User} from "./User.jsx";
+import componentsImg from './assets/components.png';
+import configImg from './assets/config.png';
+import stateMgmtImg from './assets/state-mgmt.png';
 
 // Dynamic description
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
@@ -21,14 +23,43 @@ function Header() {
     </header>
   );
 }
+// Components props
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
 
 function App() {
   return (
     <div>
       <Header></Header>
       <main>
-        <h2>Time to get started!</h2>
-        <User />
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="The core UI building block."
+              image={componentsImg}
+            />
+            <CoreConcept
+              title="Config"
+              description="Config building block."
+              image={configImg}
+            />
+            <CoreConcept
+              title="State Management"
+              description="State Management block."
+              image={stateMgmtImg}
+            />
+
+          </ul>
+        </section>
       </main>
     </div>
   );
