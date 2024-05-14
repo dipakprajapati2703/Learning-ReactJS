dcfb80be6c443ed8744aa27a94ca8c04b4de41d4
dipakp-logicrays <dipakp@logicrays.com>
2024-05-14 17:42:17 +0530

    React essential component events - 04 how-not-to-update-ui

--- code/06-React-Essentials/03-component-events/src/App.jsx
+++ code/06-React-Essentials/03-component-events/src/App.jsx
@@ -6,10 +6,14 @@ import { CoreConcept, CoreConcept2 } from './components/CoreConcept/CoreConcept.
 import TabButton from './components/TabButton.jsx';
 
 function App() {
+  let tabContent = 'Please click a button';
   function handleSelect(selectedButton) {
     // selectedButton => 'components', 'jsx', 'props', 'state'
-    console.log(selectedButton);
+    tabContent = selectedButton;
+    console.log(tabContent);
   }
+
+  console.log('APP COMPONENT EXECUTING');
 
   return (
     <div>
@@ -42,6 +46,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
+          {tabContent}
         </section>
       </main>
     </div>
